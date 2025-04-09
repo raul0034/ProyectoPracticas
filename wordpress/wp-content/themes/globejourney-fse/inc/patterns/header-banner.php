9bd615b10bb9593c7dbf3cbcc110e40c421d81e9
pedrodelarua <pdl0003@alu.medac.es>
2025-04-09 10:14:44 +0200

    resubido de la pagina

---
+++ wordpress/wp-content/themes/globejourney-fse/inc/patterns/header-banner.php
@@ -0,0 +1,27 @@
+<?php  
+/**
+ * Default Header Banner
+ */
+return array(
+	'title'      => esc_html__( 'Header Banner', 'globejourney-fse' ),
+	'categories' => array( 'globejourney-fse', 'Header Banner' ),
+	'content'    => '<!-- wp:group {"style":{"spacing":{"padding":{"top":"0","right":"0","bottom":"0","left":"0"},"blockGap":"0","margin":{"top":"0","bottom":"0"}},"dimensions":{"minHeight":"0px"}},"layout":{"type":"default"}} -->
+<div class="wp-block-group" style="min-height:0px;margin-top:0;margin-bottom:0;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:cover {"url":"'.esc_url(get_template_directory_uri()).'/assets/images/header-banner.jpg","id":3139,"dimRatio":0,"customOverlayColor":"#f4a38a","isUserOverlayColor":false,"minHeight":588,"isDark":false,"className":"hdrbanner-BX","style":{"spacing":{"padding":{"top":"var:preset|spacing|50","right":"var:preset|spacing|50","bottom":"var:preset|spacing|50","left":"var:preset|spacing|50"},"margin":{"top":"0","bottom":"0"},"blockGap":"0"}}} -->
+<div class="wp-block-cover is-light hdrbanner-BX" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50);min-height:588px"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim" style="background-color:#f4a38a"></span><img class="wp-block-cover__image-background wp-image-3139" alt="" src="'.esc_url(get_template_directory_uri()).'/assets/images/header-banner.jpg" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:group {"className":"bannerInfo","style":{"spacing":{"padding":{"top":"0","right":"0","bottom":"0","left":"0"},"blockGap":"0","margin":{"top":"0","bottom":"0"}}},"layout":{"type":"constrained","contentSize":"1170px","justifyContent":"center"}} -->
+<div class="wp-block-group bannerInfo" style="margin-top:0;margin-bottom:0;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:heading {"textAlign":"center","level":5,"style":{"typography":{"fontSize":"18px","textTransform":"uppercase","fontStyle":"normal","fontWeight":"500","lineHeight":"1.1"},"spacing":{"margin":{"top":"var:preset|spacing|70","bottom":"var:preset|spacing|40"}}},"textColor":"foreground","fontFamily":"poppins"} -->
+<h5 class="wp-block-heading has-text-align-center has-foreground-color has-text-color has-poppins-font-family" style="margin-top:var(--wp--preset--spacing--70);margin-bottom:var(--wp--preset--spacing--40);font-size:18px;font-style:normal;font-weight:500;line-height:1.1;text-transform:uppercase">NATURAL WONDER</h5>
+<!-- /wp:heading -->
+
+<!-- wp:heading {"textAlign":"center","style":{"typography":{"fontSize":"55px","textTransform":"none","lineHeight":"1.1","fontStyle":"regular","fontWeight":"600"},"spacing":{"margin":{"bottom":"var:preset|spacing|60"}}},"textColor":"foreground","fontFamily":"playfair-display"} -->
+<h2 class="wp-block-heading has-text-align-center has-foreground-color has-text-color has-playfair-display-font-family" style="margin-bottom:var(--wp--preset--spacing--60);font-size:55px;font-style:regular;font-weight:600;line-height:1.1;text-transform:none">Discover The Most Engaging and<br>Attractive Places</h2>
+<!-- /wp:heading -->
+
+<!-- wp:buttons {"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"flex","justifyContent":"center"}} -->
+<div class="wp-block-buttons"><!-- wp:button {"textAlign":"center","backgroundColor":"foreground","textColor":"heading","className":"is-style-fill","style":{"typography":{"fontSize":"17px","fontStyle":"normal","fontWeight":"500"},"spacing":{"padding":{"left":"var:preset|spacing|60","right":"var:preset|spacing|60","top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"}},"border":{"radius":"50px"}},"fontFamily":"inter"} -->
+<div class="wp-block-button has-custom-font-size is-style-fill has-inter-font-family" style="font-size:17px;font-style:normal;font-weight:500"><a class="wp-block-button__link has-heading-color has-foreground-background-color has-text-color has-background has-text-align-center wp-element-button" href="#" style="border-radius:50px;padding-top:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--60)">Read More</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div></div>
+<!-- /wp:cover --></div>
+<!-- /wp:group -->',
+);
---
+++ wordpress/wp-content/themes/globejourney-fse/inc/patterns/section1.php
@@ -0,0 +1,101 @@
+<?php
+/**
+ * Popular Tours Sections 1
+ */
+return array(
+	'title'      => esc_html__( 'Popular Tours Sections 1 ', 'globejourney-fse' ),
+	'categories' => array( 'globejourney-fse', 'Popular Tours Sections 1' ),
+	'content'    => '<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|80","bottom":"var:preset|spacing|80","left":"var:preset|spacing|50","right":"var:preset|spacing|50"}},"color":{"background":"#f7f7f3"}},"layout":{"type":"default"}} -->
+<div class="wp-block-group has-background" style="background-color:#f7f7f3;padding-top:var(--wp--preset--spacing--80);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--80);padding-left:var(--wp--preset--spacing--50)"><!-- wp:group {"layout":{"type":"constrained","contentSize":"1170px"}} -->
+<div class="wp-block-group"><!-- wp:heading {"textAlign":"center","level":5,"style":{"typography":{"fontSize":"18px"},"elements":{"link":{"color":{"text":"var:preset|color|primary"}}}},"textColor":"primary","fontFamily":"poppins"} -->
+<h5 class="wp-block-heading has-text-align-center has-primary-color has-text-color has-link-color has-poppins-font-family" style="font-size:18px">Travel Category</h5>
+<!-- /wp:heading -->
+
+<!-- wp:heading {"textAlign":"center","style":{"typography":{"fontSize":"44px","fontStyle":"regular","fontWeight":"600"},"elements":{"link":{"color":{"text":"#100201"}}},"color":{"text":"#100201"},"spacing":{"margin":{"top":"var:preset|spacing|20","bottom":"var:preset|spacing|70"}}},"fontFamily":"playfair-display"} -->
+<h2 class="wp-block-heading has-text-align-center has-text-color has-link-color has-playfair-display-font-family" style="color:#100201;margin-top:var(--wp--preset--spacing--20);margin-bottom:var(--wp--preset--spacing--70);font-size:44px;font-style:regular;font-weight:600">Our Popular Tours Type</h2>
+<!-- /wp:heading -->
+
+<!-- wp:columns {"style":{"spacing":{"blockGap":{"left":"var:preset|spacing|50"}}}} -->
+<div class="wp-block-columns"><!-- wp:column {"verticalAlignment":"top","className":"popular_tourcol","style":{"spacing":{"blockGap":"0"}}} -->
+<div class="wp-block-column is-vertically-aligned-top popular_tourcol"><!-- wp:cover {"url":"'.esc_url(get_template_directory_uri()).'/assets/images/globe-services01.jpg","id":3182,"dimRatio":0,"customOverlayColor":"#5987b4","isUserOverlayColor":false,"minHeight":346,"className":"coverBX","style":{"border":{"radius":"15px"}},"layout":{"type":"default"}} -->
+<div class="wp-block-cover coverBX" style="border-radius:15px;min-height:346px"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim" style="background-color:#5987b4"></span><img class="wp-block-cover__image-background wp-image-3182" alt="" src="'.esc_url(get_template_directory_uri()).'/assets/images/globe-services01.jpg" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:group {"className":"tourcaption_box","style":{"elements":{"link":{"color":{"text":"var:preset|color|body-text"}}},"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50","left":"var:preset|spacing|40","right":"var:preset|spacing|40"},"margin":{"top":"0rem","bottom":"0"}},"border":{"radius":"15px"}},"backgroundColor":"foreground","textColor":"body-text","layout":{"type":"default"}} -->
+<div class="wp-block-group tourcaption_box has-body-text-color has-foreground-background-color has-text-color has-background has-link-color" style="border-radius:15px;margin-top:0rem;margin-bottom:0;padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--40)"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between","verticalAlignment":"center"}} -->
+<div class="wp-block-group"><!-- wp:heading {"level":5,"style":{"typography":{"fontSize":"20px","lineHeight":"1.2"},"color":{"text":"#282828"},"elements":{"link":{"color":{"text":"#282828"}}}},"fontFamily":"playfair-display"} -->
+<h5 class="wp-block-heading has-text-color has-link-color has-playfair-display-font-family" style="color:#282828;font-size:20px;line-height:1.2">Adventure Tour</h5>
+<!-- /wp:heading -->
+
+<!-- wp:image {"lightbox":{"enabled":false},"id":3197,"sizeSlug":"full","linkDestination":"custom"} -->
+<figure class="wp-block-image size-full"><a href="#"><img src="'.esc_url(get_template_directory_uri()).'/assets/images/icon_more.png" alt="" class="wp-image-3197"/></a></figure>
+<!-- /wp:image --></div>
+<!-- /wp:group -->
+
+<!-- wp:paragraph {"className":"gpara"} -->
+<p class="gpara">Perspi ciatis under an omnis iste natus.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div></div>
+<!-- /wp:cover --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"top","className":"popular_tourcol"} -->
+<div class="wp-block-column is-vertically-aligned-top popular_tourcol"><!-- wp:cover {"url":"'.esc_url(get_template_directory_uri()).'/assets/images/globe-services02.jpg","id":3211,"dimRatio":0,"customOverlayColor":"#8998b9","isUserOverlayColor":false,"minHeight":346,"isDark":false,"style":{"border":{"radius":"15px"}},"layout":{"type":"default"}} -->
+<div class="wp-block-cover is-light" style="border-radius:15px;min-height:346px"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim" style="background-color:#8998b9"></span><img class="wp-block-cover__image-background wp-image-3211" alt="" src="'.esc_url(get_template_directory_uri()).'/assets/images/globe-services02.jpg" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:group {"className":"tourcaption_box","style":{"elements":{"link":{"color":{"text":"var:preset|color|body-text"}}},"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50","left":"var:preset|spacing|40","right":"var:preset|spacing|40"},"margin":{"top":"0rem"}},"border":{"radius":"15px"}},"backgroundColor":"foreground","textColor":"body-text","layout":{"type":"default"}} -->
+<div class="wp-block-group tourcaption_box has-body-text-color has-foreground-background-color has-text-color has-background has-link-color" style="border-radius:15px;margin-top:0rem;padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--40)"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group"><!-- wp:heading {"level":5,"style":{"typography":{"fontSize":"20px","lineHeight":"1.2"},"color":{"text":"#282828"},"elements":{"link":{"color":{"text":"#282828"}}}},"fontFamily":"playfair-display"} -->
+<h5 class="wp-block-heading has-text-color has-link-color has-playfair-display-font-family" style="color:#282828;font-size:20px;line-height:1.2">Cruises Tour</h5>
+<!-- /wp:heading -->
+
+<!-- wp:image {"lightbox":{"enabled":false},"id":3197,"sizeSlug":"full","linkDestination":"custom"} -->
+<figure class="wp-block-image size-full"><a href="#"><img src="'.esc_url(get_template_directory_uri()).'/assets/images/icon_more.png" alt="" class="wp-image-3197"/></a></figure>
+<!-- /wp:image --></div>
+<!-- /wp:group -->
+
+<!-- wp:paragraph {"className":"gpara"} -->
+<p class="gpara">Perspi ciatis under an omnis iste natus.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div></div>
+<!-- /wp:cover --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"top","className":"popular_tourcol"} -->
+<div class="wp-block-column is-vertically-aligned-top popular_tourcol"><!-- wp:cover {"url":"'.esc_url(get_template_directory_uri()).'/assets/images/globe-services03.jpg","id":3212,"dimRatio":0,"customOverlayColor":"#7e7a7e","isUserOverlayColor":false,"minHeight":346,"style":{"border":{"radius":"15px"}},"layout":{"type":"default"}} -->
+<div class="wp-block-cover" style="border-radius:15px;min-height:346px"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim" style="background-color:#7e7a7e"></span><img class="wp-block-cover__image-background wp-image-3212" alt="" src="'.esc_url(get_template_directory_uri()).'/assets/images/globe-services03.jpg" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:group {"className":"tourcaption_box","style":{"elements":{"link":{"color":{"text":"var:preset|color|body-text"}}},"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50","left":"var:preset|spacing|40","right":"var:preset|spacing|40"},"margin":{"top":"0rem"}},"border":{"radius":"15px"}},"backgroundColor":"foreground","textColor":"body-text","layout":{"type":"default"}} -->
+<div class="wp-block-group tourcaption_box has-body-text-color has-foreground-background-color has-text-color has-background has-link-color" style="border-radius:15px;margin-top:0rem;padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--40)"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group"><!-- wp:heading {"level":5,"style":{"typography":{"fontSize":"20px","lineHeight":"1.2"},"color":{"text":"#282828"},"elements":{"link":{"color":{"text":"#282828"}}}},"fontFamily":"playfair-display"} -->
+<h5 class="wp-block-heading has-text-color has-link-color has-playfair-display-font-family" style="color:#282828;font-size:20px;line-height:1.2">Hiking Tour</h5>
+<!-- /wp:heading -->
+
+<!-- wp:image {"lightbox":{"enabled":false},"id":3197,"sizeSlug":"full","linkDestination":"custom"} -->
+<figure class="wp-block-image size-full"><a href="#"><img src="'.esc_url(get_template_directory_uri()).'/assets/images/icon_more.png" alt="" class="wp-image-3197"/></a></figure>
+<!-- /wp:image --></div>
+<!-- /wp:group -->
+
+<!-- wp:paragraph {"className":"gpara"} -->
+<p class="gpara">Perspi ciatis under an omnis iste natus.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div></div>
+<!-- /wp:cover --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"top","className":"popular_tourcol"} -->
+<div class="wp-block-column is-vertically-aligned-top popular_tourcol"><!-- wp:cover {"url":"'.esc_url(get_template_directory_uri()).'/assets/images/globe-services04.jpg","id":3213,"dimRatio":0,"customOverlayColor":"#8b7574","isUserOverlayColor":false,"minHeight":346,"style":{"border":{"radius":"15px"}},"layout":{"type":"default"}} -->
+<div class="wp-block-cover" style="border-radius:15px;min-height:346px"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim" style="background-color:#8b7574"></span><img class="wp-block-cover__image-background wp-image-3213" alt="" src="'.esc_url(get_template_directory_uri()).'/assets/images/globe-services04.jpg" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:group {"className":"tourcaption_box","style":{"elements":{"link":{"color":{"text":"var:preset|color|body-text"}}},"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50","left":"var:preset|spacing|40","right":"var:preset|spacing|40"},"margin":{"top":"0rem","bottom":"0px"}},"border":{"radius":"15px"}},"backgroundColor":"foreground","textColor":"body-text","layout":{"type":"default"}} -->
+<div class="wp-block-group tourcaption_box has-body-text-color has-foreground-background-color has-text-color has-background has-link-color" style="border-radius:15px;margin-top:0rem;margin-bottom:0px;padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--40)"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group"><!-- wp:heading {"level":5,"style":{"typography":{"fontSize":"20px","lineHeight":"1.2"},"color":{"text":"#282828"},"elements":{"link":{"color":{"text":"#282828"}}}},"fontFamily":"playfair-display"} -->
+<h5 class="wp-block-heading has-text-color has-link-color has-playfair-display-font-family" style="color:#282828;font-size:20px;line-height:1.2">Honeymoon Tour</h5>
+<!-- /wp:heading -->
+
+<!-- wp:image {"lightbox":{"enabled":false},"id":3197,"sizeSlug":"full","linkDestination":"custom"} -->
+<figure class="wp-block-image size-full"><a href="#"><img src="'.esc_url(get_template_directory_uri()).'/assets/images/icon_more.png" alt="" class="wp-image-3197"/></a></figure>
+<!-- /wp:image --></div>
+<!-- /wp:group -->
+
+<!-- wp:paragraph {"className":"gpara"} -->
+<p class="gpara">Perspi ciatis under an omnis iste natus.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div></div>
+<!-- /wp:cover --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->',
+);
