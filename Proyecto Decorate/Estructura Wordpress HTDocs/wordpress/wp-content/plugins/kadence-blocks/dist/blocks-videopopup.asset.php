b630352fa79897db55f3a8eaf8e54d80d2358f68
pedrodelarua <pdl0003@alu.medac.es>
2025-04-29 09:43:20 +0200

    Eliminado todo lo que no es de la web.

--- Proyecto Decorate/Estructura Wordpress HTDocs/wordpress/wp-content/plugins/kadence-blocks/dist/blocks-videopopup.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('kadence-components', 'kadence-helpers', 'kadence-icons', 'lodash', 'react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => 'a27ddf8338e9ed68acd0');
--- Proyecto Decorate/Estructura Wordpress HTDocs/wordpress/wp-content/plugins/kadence-blocks/dist/helpers.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('kadence-icons', 'lodash', 'wp-api-fetch', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-widgets'), 'version' => '50a832e22b94d7057dd1');
